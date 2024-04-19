0a3b5973fe78946a966ca0c5a37142c2e05987d9
John Waithaka <johnwaruiwaithaka@gmail.com>
2024-04-19 15:11:09 +0300

    success

--- index.js
+++ index.js
@@ -1,3 +1,21 @@
-const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
+//const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
 // Code your solution here
+//calculate the total number of battery batches.
+// function totalBatteries(batteries) {
+//     const batteryBatches = batteries.length;
+//   }
+
+
+
+const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
+
+const totalBatteries = batteryBatches.reduce(function(total, batches){
+  return batches + total
+  }
+)
+
+// const batteriesInBatch
+// const batteries
+// const batteryBatches
+// const totalBatteries
